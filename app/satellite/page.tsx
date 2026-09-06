464d081f1306da5a40b8194237fa49956e9a7fff
salehshahab999-gif <salehshahab999@gmail.com>
2026-09-06 21:46:04 +0330

    feat: mount geographic map search on satellite page

--- app/satellite/page.tsx
+++ app/satellite/page.tsx
@@ -1,10 +1,12 @@
 import MaritimeSkeletonOverlay from "../components/MaritimeSkeletonOverlay";
+import MapSearchPanel from "../components/MapSearchPanel";
 import SatelliteIntelligence from "../components/SatelliteIntelligence";
 
 export default function SatellitePage() {
   return (
     <>
       <SatelliteIntelligence />
+      <MapSearchPanel />
       <MaritimeSkeletonOverlay />
     </>
   );
