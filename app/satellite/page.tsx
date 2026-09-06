2b1028eee58113b51ea766b1fc029a13367c2575
salehshahab999-gif <salehshahab999@gmail.com>
2026-09-06 19:58:42 +0330

    feat: add standalone satellite intelligence test route

---
+++ app/satellite/page.tsx
@@ -0,0 +1,11 @@
+import SatelliteIntelligence from "../components/SatelliteIntelligence";
+
+export default function SatellitePage() {
+  return (
+    <main className="min-h-screen bg-black p-6 text-white">
+      <div className="mx-auto max-w-[1800px]">
+        <SatelliteIntelligence />
+      </div>
+    </main>
+  );
+}
