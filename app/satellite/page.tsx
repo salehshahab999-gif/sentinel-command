16ea1516fc421d796d337736c3737159c15f7be7
salehshahab999-gif <salehshahab999@gmail.com>
2026-09-06 20:50:13 +0330

    feat: attach maritime skeleton overlay to satellite view

--- app/satellite/page.tsx
+++ app/satellite/page.tsx
@@ -1,5 +1,11 @@
+import MaritimeSkeletonOverlay from "../components/MaritimeSkeletonOverlay";
 import SatelliteIntelligence from "../components/SatelliteIntelligence";
 
 export default function SatellitePage() {
-  return <SatelliteIntelligence />;
+  return (
+    <>
+      <SatelliteIntelligence />
+      <MaritimeSkeletonOverlay />
+    </>
+  );
 }
